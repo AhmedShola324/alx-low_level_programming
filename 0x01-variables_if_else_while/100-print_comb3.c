adf647e90568403f757f0349adc5ee1620e76bed
AhmedShola324 <sholabodunrin@gmail.com>
2022-03-12 08:20:22 -0800

    unique combination between 1 and 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+* main - prints the alphabets in lower and  capital letters
+*
+*Description: This function prints the lowerand upper case characters
+*using the putchar function
+*Return: prints the alphabets followed by a new line
+*/
+int main(void)
+{
+	int i = 48;
+	int e = 48;
+
+		while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+			
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
