286a518cfe387dca785ca224751999b01a8db42f
AhmedShola324 <sholabodunrin@gmail.com>
2022-03-12 06:50:29 -0800

    print numbers from 0, to 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - prints the alphabets in lower and  capital letters
+*
+*Description: This function prints the lowerand upper case characters
+*using the putchar function
+*Return: prints the alphabets followed by a new line
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i != 58)
+	{ 
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
