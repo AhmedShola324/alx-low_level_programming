4d6f0a2cd28e12b07042fbe63e9da2fd0ffeacde
AhmedShola324 <sholabodunrin@gmail.com>
2022-03-12 09:50:19 -0800

    fixed logical error on line 11

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - This is a very important function
+*
+* Description: Prints all possible combinations of two-digit numbers
+* Return: Returns the output of the main function.
+*/
+int main(void)
+{	int i;
+	int j;
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
