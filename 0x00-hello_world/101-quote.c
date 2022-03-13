fcbe48690b8748939747037151d26cf0be75e9d6
AhmedShola324 <sholabodunrin@gmail.com>
2022-03-13 10:25:38 -0700

    print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print a phrase without using printf and puts to the 
+ * standard error
+ *
+ * Description: This program uses write to solve this problem
+ * Return: 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
