ad2b09e3d64ab6d9079b3367467193c97722e142
AhmedShola324 <sholabodunrin@gmail.com>
2022-03-23 15:41:41 -0700

    task 8 solution

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int n;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+	n = i / 2;
+
+	while ( str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
