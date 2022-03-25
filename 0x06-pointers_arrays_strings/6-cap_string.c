adce65dd0a331e01beb455bd750d2f09b8c2c720
AhmedShola324 <sholabodunrin@gmail.com>
2022-03-24 17:14:31 -0700

    task 6 solution

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * cap_string - capitalize all words of a string
+ * @str: string
+ * Return: string
+ */
+char *cap_string(char *str)
+{
+	int i, c, d;
+
+	char n[] = ",;.!?(){}\n\t\" ";
+
+	for (i = 0, d = 0; str[i] != '\0'; i++)
+	{
+		if (str[0] > 96 && str[0] < 123)
+		{
+			d = 1;
+		}
+		for (c = 0; n[c] != '\0'; c++)
+		{
+			if (n[c] == str[i])
+				d = 1;
+		}
+		if (d)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				d = 0;
+			}
+			else if (str[i] > 64 && str[i] < 91)
+			{
+				d = 0;
+			}
+			else if (str[i] > 47 && str[i] < 58)
+			{
+				d = 0;
+			}
+		}
+	}
+	return (0);
+}
